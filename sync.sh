93324e8d978a28ffd97507274d4fa09162118bb9
l-qing <190219044@qq.com>
2022-07-07 13:34:01 +0800

    chore: sync from remote

--- sync.sh
+++ sync.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
-curl -fLo YouTube.sgmodule https://raw.githubusercontent.com/jimmyorz/Surge/master/YouTube.sgmodule
+# curl -fLo YouTube.sgmodule https://raw.githubusercontent.com/jimmyorz/Surge/master/YouTube.sgmodule
+
+curl -fLo YouTube.sgmodule https://raw.githubusercontent.com/nzw9314/Surge/master/Module/Script/youtube.sgmodule
 
